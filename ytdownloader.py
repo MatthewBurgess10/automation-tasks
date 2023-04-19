f096330c88b10bb1951f1fb33f3e8b0d9545f09f
Matthew Burgess <matthew456burgess@btinternet.com>
2023-04-19 20:03:18 +0100

    added better exception handling

--- ytdownloader.py
+++ ytdownloader.py
@@ -2,15 +2,15 @@
 from pytube import YouTube
 from sys import argv
 
-link = argv[1]
-yt = YouTube(link)
-
-print("Title: ", yt.title)
-print("View: ", yt.views)
-
-yd = yt.streams.get_highest_resolution()
-
-yd.download('/Users/Matthew/Downloads/YT vid downloads')
+try:
+    link = argv[1]
+    yt = YouTube(link)
+    print("Title: ", yt.title)
+    print("View: ", yt.views)
+    yd = yt.streams.get_highest_resolution()
+    yd.download('/Users/Matthew/Downloads/YT vid downloads')
+except Exception as e:
+    print("An error occurred:", e)
 
 
 #to run the program do this:
