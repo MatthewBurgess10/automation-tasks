2467432258873d35ae2c19635dbfe356473b3d53
Matthew Burgess <matthew456burgess@btinternet.com>
2023-05-17 19:42:47 +0100

    added a internet speed tester file

--- internetspeedtester.py
+++ internetspeedtester.py
@@ -1,7 +1,17 @@
-import speedtest as st
+import speedtest
 
-#set best server 
-server = st.Speedtest()
-server.get_best_server()
+st = speedtest.Speedtest()
 
-#test download speed 
+option = int(input("What speed do you want to test:\n1)Download speed \n2)Upload speed\n3)Ping\nYour choice:"))
+                   
+if option == 1:
+    print(st.download())
+elif option == 2:
+    print(st.upload())
+elif option ==3:
+    servername = []
+    st.get_servers(servername)
+    print(st.results.ping)
+
+else:
+    print("That is not a valid entry, please try again")
