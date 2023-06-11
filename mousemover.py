ddcba64c6543909a8109e477bb2fc818c0e0a6d2
Matthew Burgess <matthew456burgess@btinternet.com>
2023-06-11 11:25:50 +0100

    Started code-few bugs

--- mousemover.py
+++ mousemover.py
@@ -1,1 +1,13 @@
 #this file is for a program that moves the mouse around automatically
+import pyautogui
+import sys
+import keyboard
+
+run = True
+
+while run == True:
+    pyautogui.move(200, 200)
+    pyautogui.move(None, 300)
+    if keyboard.is_pressed('Esc'):
+        sys.exit(0)
+        
